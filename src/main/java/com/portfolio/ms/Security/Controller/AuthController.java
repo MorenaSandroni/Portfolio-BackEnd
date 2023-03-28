4533a177781c3e9c00baf872acbebe57753e7b89
Morena <morenasandroni.b@gmail.com>
2023-03-28 00:06:57 -0300

    [ADD] Estructura

--- src/main/java/com/portfolio/ms/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ms/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://msfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-arg-programa.onrender.com","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
